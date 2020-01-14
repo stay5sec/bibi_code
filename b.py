e78b7e05f692ef83344502d58f7b53ad5e4ec25c
stay5sec <bilibili_super@qq.com>
2020-01-14 16:38:50 +0800

    0114

---
+++ b.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+# author:Super
+import pandas as pd
+import datetime
+
+# desk path：/Users/super/Desktop/
+
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.max_rows', 1000)
+
+print(1)
+
+print(2)
