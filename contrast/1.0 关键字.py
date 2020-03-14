b046145a74b8e240d8b6c3a4b2c314e5071fb414
stay5sec <bilibili_super@qq.com>
2020-03-14 23:05:52 +0800

    0314

---
+++ contrast/1.0 关键字.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+# author:stay5sec
+
+def fx():
+    print("hello world")
+
+
+fx()
---
+++ contrast/1.1 变量.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+# author:stay5sec
+
+# n = "a"
+#
+# n1="abc"
+#
+# # n = "n"
+#
+# print(n,n1)
+
+
+a, b, c = 1, 2, 3
+
+print(a, b, c)
+
+'''String name="爱慕课";
+        char sex='男';
+        int num=18;
+        double price=120.5;
+        boolean isOK=true;
+		System.out.println(name);
+		System.out.println(sex);
+		System.out.println(num);
+		System.out.println(price);
+		System.out.println(isOK);'''
