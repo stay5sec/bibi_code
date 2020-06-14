4efe6b65ee269504c73774951df7ff9278c64b4c
stay5sec <bilibili_super@qq.com>
2020-06-15 07:01:58 +0800

    0615

--- other/作图/密度图.py
+++ other/作图/密度图.py
@@ -14,26 +14,30 @@ pd.set_option('display.max_rows', 1000)
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.datasets import load_iris
 
-iris = load_iris()
-
-# 获取特征集和分类标识
-features = iris.data
-labels = iris.target
-
-# 花萼长度、花萼宽度、花瓣长度、花瓣宽度
-
-df=pd.DataFrame(features)
-df["labels"]=labels
-
-df.columns=["花萼长度","花萼宽度","花瓣长度","花瓣宽度","种类"]
-
-print(df.head())
+# from sklearn.datasets import load_iris
+#
+# iris = load_iris()
+#
+# # 获取特征集和分类标识
+# features = iris.data
+# labels = iris.target
+#
+# # 花萼长度、花萼宽度、花瓣长度、花瓣宽度
+#
+# df=pd.DataFrame(features)
+# df["labels"]=labels
+#
+# df.columns=["花萼长度","花萼宽度","花瓣长度","花瓣宽度","种类"]
+#
+# print(df.head())
 # exit()
+tips = sns.load_dataset("tips")
+plt.figure(figsize=(14,8))
+sns.kdeplot(tips[["total_bill","tip"]],shade=True,shade_lowest=False)
+plt.grid()
 
-
-sns.pairplot(df)
+# sns.pairplot(df)
 plt.show()
 
 
